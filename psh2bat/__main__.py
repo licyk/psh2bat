e08f92774dc09b25662dd8b9ec234c0f9baf3a44
licyk <licyk233@163.com>
2026-03-02 23:57:57 +0800

    refactor(psh2bat): remove redundant main module
    BREAKING CHANGE: Removed psh2bat/main.py module and changed entry point
    from psh2bat.main:main to psh2bat.app:run_app in pyproject.toml.

--- psh2bat/__main__.py
+++ psh2bat/__main__.py
@@ -2,11 +2,5 @@
 
 from psh2bat.app import run_app
 
-
-def main() -> None:
-    """主函数"""
-    run_app()
-
-
 if __name__ == "__main__":
-    main()
+    run_app()
--- psh2bat/version.py
+++ psh2bat/version.py
@@ -1,3 +1,3 @@
 """Psh2Bat 版本"""
 
-VERSION = "0.0.4"
+VERSION = "0.0.5"
